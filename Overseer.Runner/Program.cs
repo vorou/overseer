5717ea3f1841a5df565d2a6e54b2691b62b55a09
vorou <vorou@skbkontur.ru>
2014-02-18 00:08:31 +0600

    runner

---
+++ Overseer.Runner/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Overseer.Runner
+{
+    public class Program
+    {
+        private static void Main()
+        {
+            var sourceIndexer = new SourceIndexer();
+            var sourceRepository = new SourceRepository();
+            var sw = Stopwatch.StartNew();
+            foreach (var source in sourceIndexer.Index(@"W:\ftp"))
+            {
+                sourceRepository.Save(source);
+            }
+            Console.Out.WriteLine(sw.Elapsed.TotalMinutes);
+        }
+    }
+}
