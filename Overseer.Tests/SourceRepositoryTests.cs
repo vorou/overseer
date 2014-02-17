3bf6095c4a0f9d445bb2b6da27c11eb3f9e8c34d
vorou <vorou@skbkontur.ru>
2014-02-17 23:22:46 +0600

    test for repo

---
+++ Overseer.Tests/SourceRepositoryTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Shouldly;
+
+namespace Overseer.Tests
+{
+    public class SourceRepositoryTests
+    {
+        [Test]
+        public void Saving_Always_ShouldSaveSource()
+        {
+            var source = new Source {Id = "panda", TenderId = "123", Type = "bear"};
+            var sut = new SourceRepository();
+
+            sut.Save(source);
+
+            var actual = sut.FindAll().Single();
+            actual.Id.ShouldBe(source.Id);
+            actual.TenderId.ShouldBe(source.TenderId);
+            actual.Type.ShouldBe(source.Type);
+        }
+    }
+}
---
+++ Overseer/SourceRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using Nest;
+
+namespace Overseer
+{
+    public class SourceRepository
+    {
+        private readonly ElasticClient elastic;
+
+        public SourceRepository()
+        {
+            elastic = new ElasticClient(new ConnectionSettings(new Uri("http://localhost:9200")));
+        }
+
+        public void Save(Source source)
+        {
+            elastic.Index(source);
+        }
+
+        public IEnumerable<Source> FindAll()
+        {
+            return elastic.Search<Source>(s=>s.AllTypes()).Documents;
+        }
+    }
+}
