a10d832fa640f836d95c9aea74f10e219918cf52
vorou <vorou@skbkontur.ru>
2014-02-25 22:11:34 +0600

    extract path to config

--- src/Overseer.Importer/Program.cs
+++ src/Overseer.Importer/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Overseer.Importer
+﻿using System.Configuration;
+
+namespace Overseer.Importer
 {
     public class Program
     {
         private static void Main()
         {
-            var sourceIndexer = new TenderReader(new FileReader(@"W:\ftp"));
+            var sourceIndexer = new TenderReader(new FileReader(ConfigurationManager.AppSettings["src-dir"]));
             var sourceRepository = new TenderRepository("overseer");
             sourceRepository.Clear();
             foreach (var source in sourceIndexer.Read())
