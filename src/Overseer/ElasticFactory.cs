ec984f03bb9fa3fc4d9d1238e6afbd5bd4bdad54
vorou <vorou@skbkontur.ru>
2014-02-27 23:55:23 +0600

    elastic factory

---
+++ src/Overseer/ElasticFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Nest;
+
+namespace Overseer
+{
+    public static class ElasticFactory
+    {
+        public static ElasticClient CreateClient(string cs, string index)
+        {
+            return new ElasticClient(new ConnectionSettings(new Uri(cs)).SetDefaultIndex(index));
+        }
+    }
+}
