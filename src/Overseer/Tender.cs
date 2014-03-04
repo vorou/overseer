67a0027cffca4e39c0138e236c28843cd4e13587
vorou <vorou@skbkontur.ru>
2014-03-04 23:09:13 +0600

    dont need to analyze id & type

--- src/Overseer/Tender.cs
+++ src/Overseer/Tender.cs
@@ -1,10 +1,13 @@
 using System;
+using Nest;
 
 namespace Overseer
 {
     public class Tender
     {
+        [ElasticProperty(Index = FieldIndexOption.not_analyzed)]
         public string Id { get; set; }
+        [ElasticProperty(Index = FieldIndexOption.not_analyzed)]
         public string Type { get; set; }
         public decimal TotalPrice { get; set; }
         public string Name { get; set; }
