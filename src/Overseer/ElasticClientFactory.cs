2bd79b4da9e4e69fd145efe7e31e6002ef25e41d
vorou <vorou@skbkontur.ru>
2014-03-06 01:05:18 +0600

    logging

--- src/Overseer/ElasticClientFactory.cs
+++ src/Overseer/ElasticClientFactory.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Configuration;
+using log4net;
 using Nest;
 
 namespace Overseer
 {
     public static class ElasticClientFactory
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof (ElasticClientFactory));
+
         public static ElasticClient Create()
         {
-            return
-                new ElasticClient(
-                    new ConnectionSettings(new Uri(ConfigurationManager.AppSettings["esHost"])).SetDefaultIndex(ConfigurationManager.AppSettings["esIndex"]));
+            var esHost = ConfigurationManager.AppSettings["esHost"];
+            log.DebugFormat("esHost = {0}", esHost);
+            var esIndex = ConfigurationManager.AppSettings["esIndex"];
+            log.DebugFormat("esIndex = {0}", esIndex);
+            return new ElasticClient(new ConnectionSettings(new Uri(esHost)).SetDefaultIndex(esIndex));
         }
     }
 }
