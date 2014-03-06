18177f3f1a7a0d127318f15cae81c94ac28462be
vorou <vorou@skbkontur.ru>
2014-03-06 18:45:14 +0600

    pub site and run doorkeeper from build script

--- build.cmd
+++ build.cmd
@@ -1,5 +1,10 @@
 @echo off
 set baseDir=%cd%
+set configuration=Release
+
+msbuild %baseDir%\src\Overseer.WebApp\Overseer.WebApp.csproj /p:Configuration=%Configuration% /p:PublishProfile=localhost /p:DeployOnBuild=true
+
 rmdir /q /s %baseDir%\bin
 mkdir %baseDir%\bin
-msbuild %baseDir%\src\Overseer.Doorkeeper\Overseer.Doorkeeper.csproj /p:Configuration=Release /p:OutputPath=%baseDir%\bin /v:minimal /nologo
+msbuild %baseDir%\src\Overseer.Doorkeeper\Overseer.Doorkeeper.csproj /p:Configuration=%Configuration% /p:OutputPath=%baseDir%\bin /v:minimal /nologo
+%baseDir%\bin\Overseer.Doorkeeper.exe
