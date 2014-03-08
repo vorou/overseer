7677c16222608b42e5802c64259340914883064c
vorou <vorou@skbkontur.ru>
2014-03-08 16:18:02 +0600

    put testable FileReader in its own file to make TestCop happy

---
+++ src/Overseer.Doorkeeper.Tests/FileReaderTestable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net;
+
+namespace Overseer.Doorkeeper.Tests
+{
+    internal class FileReaderTestable : FileReader
+    {
+        public FileReaderTestable(Uri ftp)
+            : base(ftp)
+        {
+        }
+
+        public Func<WebClient, Uri, byte[]> GetFileCoreBody { private get; set; }
+
+        protected override byte[] GetFileCore(WebClient request, Uri uri)
+        {
+            return GetFileCoreBody(request, uri);
+        }
+    }
+}
--- src/Overseer.Doorkeeper.Tests/FileReaderTests.cs
+++ src/Overseer.Doorkeeper.Tests/FileReaderTests.cs
@@ -211,20 +211,6 @@ namespace Overseer.Doorkeeper.Tests
             }
         }
 
-        private class FileReaderTestable : FileReader
-        {
-            public FileReaderTestable(Uri ftp) : base(ftp)
-            {
-            }
-
-            public Func<WebClient, Uri, byte[]> GetFileCoreBody { private get; set; }
-
-            protected override byte[] GetFileCore(WebClient request, Uri uri)
-            {
-                return GetFileCoreBody(request, uri);
-            }
-        }
-
         private FileReader CreateSut()
         {
             return new FileReader(new Uri("ftp://localhost"));
