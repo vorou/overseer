14001e5f4f0f9a4e798d7d97d3cb984e1a578b1f
vorou <vorou@skbkontur.ru>
2014-03-10 01:52:21 +0600

    use client-side templating

--- src/Overseer.WebApp/Content/js/app.js
+++ src/Overseer.WebApp/Content/js/app.js
@@ -3,3 +3,17 @@
 //App.Router.map(function () {
 //    this.resource('todos', { path: '/' });
 //});
+
+App.IndexRoute = Ember.Route.extend({
+    model: function() {
+        return tenders;
+    }
+});
+
+var tenders = [
+{
+    price: "1 999",
+    url: "www.ya.ru",
+    name: "Hello!",
+    regionName: "Region"
+}]
