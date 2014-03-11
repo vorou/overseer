40bdbe318a820fa066169fa4272e17b8cf6d73c6
vorou <vorou@skbkontur.ru>
2014-03-11 18:17:03 +0600

    real data

--- src/Overseer.WebApp/Content/js/app.js
+++ src/Overseer.WebApp/Content/js/app.js
@@ -1,19 +1,9 @@
 ﻿var App = Ember.Application.create();
 
-//App.Router.map(function () {
-//    this.resource('todos', { path: '/' });
-//});
-
 App.IndexRoute = Ember.Route.extend({
     model: function() {
-        return tenders;
+        return $.getJSON('/tenders').then(function (data){
+            return data.tenders;
+        });
     }
 });
-
-var tenders = [
-{
-    price: "1 999",
-    url: "www.ya.ru",
-    name: "Hello!",
-    regionName: "Region"
-}]
