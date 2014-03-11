f089e5b8fa2ed2da8e76dd1a71e94fa9a04e7545
vorou <vorou@skbkontur.ru>
2014-03-11 20:50:54 +0600

    make up module tests

--- src/Overseer.WebApp.Tests/OverseerModuleTests.cs
+++ src/Overseer.WebApp.Tests/OverseerModuleTests.cs
@@ -15,89 +15,80 @@ namespace Overseer.WebApp.Tests
         public void Root_Always_Responses()
         {
             var sut = CreateDefaultBrowser();
-
             sut.Get("/").StatusCode.ShouldBe(HttpStatusCode.OK);
-        }
-
-        [Fact]
-        public void Tenders_NoQuery_ContainsPriceForMostExpensiveTender()
-        {
-            var totalPriceFormatted = "123 456";
-            var totalPrice = 123456.01M;
-            var tender = fixture.Create<Tender>();
-            tender.TotalPrice = totalPrice;
-            StubMostExpensive(tender);
-
-            var actual = GetResponseAsString("/tenders");
-
-            actual.ShouldContain(totalPriceFormatted);
-        }
-
-        [Fact]
-        public void Tenders_NoQuery_ContainsNameForMostExpensiveTender()
-        {
-            var tenderName = fixture.Create<string>();
-            var tender = fixture.Create<Tender>();
-            tender.Name = tenderName;
-            StubMostExpensive(tender);
-
-            var actual = GetResponseAsString("/tenders");
-
-            actual.ShouldContain(tenderName);
-        }
-
-        [Fact]
-        public void Tenders_NoQuery_ContainsRegionNameForTender()
-        {
-            var regionNameService = fixture.Freeze<IRegionNameService>();
-            var regionId = fixture.Create<string>();
-            var regionName = fixture.Create<string>();
-            A.CallTo(() => regionNameService.GetName(regionId)).Returns(regionName);
-            var tender = fixture.Create<Tender>();
-            tender.Region = regionId;
-            StubMostExpensive(tender);
-
-            var actual = GetResponseAsString("/tenders");
-
-            actual.ShouldContain(regionName);
-        }
-
-        [Fact]
-        public void Root_Always_ContainsLinkToTender()
-        {
-            var tender = fixture.Create<Tender>();
-            tender.Id = "panda";
-            StubMostExpensive(tender);
-
-            var actual = GetResponseAsString("/tenders");
-
-            actual.ShouldContain("https://zakupki.kontur.ru/notification44?id=panda");
         }
 
         [Fact]
         public void Tenders_Always_ShouldRespond()
         {
             var sut = CreateDefaultBrowser();
-
             var actual = sut.Get("/tenders");
-
             actual.StatusCode.ShouldBe(HttpStatusCode.OK);
         }
 
         [Fact]
-        public void Tenders_QueryIsPassed_ContainsResult()
+        public void Tenders_NoQuery_ResponseContainsPriceForMostExpensiveTender()
+        {
+            var totalPrice = 123456.01M;
+            var tender = fixture.Create<Tender>();
+            tender.TotalPrice = totalPrice;
+
+            var totalPriceFormatted = "123 456";
+            AssertResponseForTenderContains(tender, totalPriceFormatted);
+        }
+
+        [Fact]
+        public void Tenders_NoQuery_ResponseContainsNameForMostExpensiveTender()
         {
             var tenderName = fixture.Create<string>();
             var tender = fixture.Create<Tender>();
             tender.Name = tenderName;
-            var repo = fixture.Freeze<ITenderRepository>();
-            A.CallTo(() => repo.Find("panda")).Returns(new[] {tender});
-            var sut = CreateDefaultBrowser();
 
-            var actual = sut.Get("/tenders", with => with.Query("q", "panda")).Body.AsString();
-
-            actual.ShouldContain(tenderName);
+            AssertResponseForTenderContains(tender, tenderName);
         }
+
+        [Fact]
+        public void Tenders_NoQuery_ResponseContainsRegionNameForMostExpensiveTender()
+        {
+            var regionNameService = fixture.Freeze<IRegionNameService>();
+            var regionId = fixture.Create<string>();
+            var regionName = fixture.Create<string>();
+            A.CallTo(() => regionNameService.GetName(regionId)).Returns(regionName);
+
+            var tender = fixture.Create<Tender>();
+            tender.Region = regionId;
+
+            AssertResponseForTenderContains(tender, regionName);
+        }
+
+        [Fact]
+        public void Tenders_NoQuery_ResponseContainsUrlForMostExpensiveTender()
+        {
+            var tender = fixture.Create<Tender>();
+            tender.Id = "panda";
+
+            var tenderUrl = "https://zakupki.kontur.ru/notification44?id=panda";
+            AssertResponseForTenderContains(tender, tenderUrl);
+        }
+
+        [Fact]
+        public void Tenders_QueryWasPassed_ResponseContainsResult()
+        {
+            var tenderName = fixture.Create<string>();
+            var tender = fixture.Create<Tender>();
+            tender.Name = tenderName;
+
+            var q = "panda";
+            var repo = fixture.Freeze<ITenderRepository>();
+            A.CallTo(() => repo.Find(q)).Returns(new[] {tender});
+
+            var sut = CreateDefaultBrowser();
+            var actual = sut.Get("/tenders", with => with.Query("q", q));
+
+            actual.Body.AsString().ShouldContain(tenderName);
+        }
+
+        private readonly IFixture fixture = new Fixture().Customize(new AutoFakeItEasyCustomization());
 
         private Browser CreateDefaultBrowser()
         {
@@ -109,17 +100,12 @@ namespace Overseer.WebApp.Tests
                                });
         }
 
-        private readonly IFixture fixture = new Fixture().Customize(new AutoFakeItEasyCustomization());
-
-        private void StubMostExpensive(Tender tender)
+        private void AssertResponseForTenderContains(Tender tender, string totalPriceFormatted)
         {
             var repo = fixture.Freeze<ITenderRepository>();
             A.CallTo(() => repo.GetMostExpensive(A<int>._)).Returns(new[] {tender});
-        }
-
-        private string GetResponseAsString(string route)
-        {
-            return CreateDefaultBrowser().Get(route).Body.AsString();
+            var actual = CreateDefaultBrowser().Get("/tenders").Body.AsString();
+            actual.ShouldContain(totalPriceFormatted);
         }
     }
 }
