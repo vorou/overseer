c2df86e5339b0a12180a7ce31b66059d61e87f10
vorou <vorou@skbkontur.ru>
2014-03-13 16:06:28 +0600

    load most expensive through jquery

---
+++ src/Overseer.WebApp/Content/js/app.js
@@ -0,0 +1,10 @@
+﻿$(function() {
+    $.getJSON("tenders", function (data) {
+        var tenders = data.tenders;
+        var tableRows = [];
+        tenders.forEach(function (tender) {
+            tableRows.push('<tr><td class="col-sm-1 price">' + tender.price + ' <i class="fa fa-rub"></i></td><td class="col-sm-4"><a href="' + tender.url + '" target="_blank">' + tender.name + ' <i class="fa fa-external-link"></i></a></td><td class="col-sm-1 region">' + tender.regionName + '</td></tr>');
+        });
+        $(tableRows.join('')).appendTo('.tender-grid');
+    });
+});
