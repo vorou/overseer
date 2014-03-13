9788c6815dde669ec5c56608c9e6424705fc8700
vorou <vorou@skbkontur.ru>
2014-03-13 15:40:09 +0600

    remove ember, use cdn for bootstrap

--- src/Overseer.WebApp/Content/js/app.js
+++
@@ -1,9 +0,0 @@
-﻿var App = Ember.Application.create();
-
-App.IndexRoute = Ember.Route.extend({
-    model: function() {
-        return $.getJSON('/tenders').then(function (data){
-            return data.tenders;
-        });
-    }
-});
