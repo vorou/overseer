45f34dcb0ae6a2da77fe3275eb5e6e14ea12a707
vorou <vorou@skbkontur.ru>
2014-03-13 21:48:01 +0600

    group tests by nested classes

--- src/Overseer.Doorkeeper.Tests/FileReaderTests.cs
+++ src/Overseer.Doorkeeper.Tests/FileReaderTests.cs
@@ -27,213 +27,222 @@ namespace Overseer.Doorkeeper.Tests
                 Directory.Delete(FtpMountDir, true);
         }
 
-        [Fact]
-        public void Read_ZipInRootDir_IgnoresIt()
+        public class FileRetrieving : FileReaderTests
         {
-            CreateZipAtFtp("", "archive.zip", "fileName");
-            var sut = CreateSut();
+            [Fact]
+            public void Read_ZipInRootDir_IgnoresIt()
+            {
+                CreateZipAtFtp("", "archive.zip", "fileName");
+                var sut = CreateSut();
 
-            var actual = sut.ReadNewFiles();
+                var actual = sut.ReadNewFiles();
 
-            actual.ShouldBeEmpty();
+                actual.ShouldBeEmpty();
+            }
+
+            [Theory]
+            [InlineData(@"fcs_regions\Adygeja_Resp\notifications\currMonth\", "archive.zip", "entry",
+                @"ftp://localhost/fcs_regions/Adygeja_Resp/notifications/currMonth/archive.zip/entry")]
+            [InlineData(@"fcs_regions\Panda_Country\notifications\currMonth\", "archive.zip", "entry",
+                @"ftp://localhost/fcs_regions/Panda_Country/notifications/currMonth/archive.zip/entry")]
+            public void Read_ZipInNotificationsCurrentMonth_PathIsUriPlusEntryName(string targetDirectory, string zipName, string entryName, string expected)
+            {
+                CreateZipAtFtp(targetDirectory, zipName, entryName);
+                var sut = CreateSut();
+
+                var actual = sut.ReadNewFiles();
+
+                actual.Single().Uri.ShouldBe(expected);
+            }
+
+            [Theory]
+            [InlineData(@"fcs_regions\Adygeja_Resp\notifications\prevMonth\", "archive.zip", "entry",
+                @"ftp://localhost/fcs_regions/Adygeja_Resp/notifications/prevMonth/archive.zip/entry")]
+            public void Read_ZipInNotificationsPrevMonth_PathIsUriPlusEntryName(string targetDirectory, string zipName, string entryName, string expected)
+            {
+                CreateZipAtFtp(targetDirectory, zipName, entryName);
+                var sut = CreateSut();
+
+                var actual = sut.ReadNewFiles();
+
+                actual.Single().Uri.ShouldBe(expected);
+            }
+
+            [Fact]
+            public void Read_ZipInNotificationsCurrentMonth_ReadsItsContent()
+            {
+                var content = "<привет></привет>";
+                CreateZipAtFtp(@"fcs_regions\Adygeja_Resp\notifications\currMonth\",
+                               "panda.zip",
+                               zip =>
+                               {
+                                   var entry = zip.CreateEntry(Path.GetRandomFileName());
+                                   using (var stream = new StreamWriter(entry.Open()))
+                                       stream.Write(content);
+                               });
+                var sut = CreateSut();
+
+                var actual = sut.ReadNewFiles();
+
+                actual.Single().Content.ShouldBe(content);
+            }
         }
 
-        [Theory]
-        [InlineData(@"fcs_regions\Adygeja_Resp\notifications\currMonth\", "archive.zip", "entry",
-            @"ftp://localhost/fcs_regions/Adygeja_Resp/notifications/currMonth/archive.zip/entry")]
-        [InlineData(@"fcs_regions\Panda_Country\notifications\currMonth\", "archive.zip", "entry",
-            @"ftp://localhost/fcs_regions/Panda_Country/notifications/currMonth/archive.zip/entry")]
-        public void Read_ZipInNotificationsCurrentMonth_PathIsUriPlusEntryName(string targetDirectory, string zipName, string entryName, string expected)
+        public class ImportControl : FileReaderTests
         {
-            CreateZipAtFtp(targetDirectory, zipName, entryName);
-            var sut = CreateSut();
+            [Fact]
+            public void ImportControl_ZipWasntMarkedAsImported_ReadsItAgain()
+            {
+                CreateZipAtFtp(@"fcs_regions\Adygeja_Resp\notifications\currMonth\", "panda.zip", Path.GetRandomFileName());
+                var sut = CreateSut();
+                sut.ReadNewFiles().ToList();
 
-            var actual = sut.ReadNewFiles();
+                var actual = sut.ReadNewFiles();
 
-            actual.Single().Uri.ShouldBe(expected);
+                actual.ShouldNotBeEmpty();
+            }
+
+            [Fact]
+            public void ImportControl_ReaderWasReset_ReadsAgain()
+            {
+                CreateZipAtFtp(@"fcs_regions\Adygeja_Resp\notifications\currMonth\", "panda.zip", Path.GetRandomFileName());
+                var sut = CreateSut();
+                sut.ReadNewFiles().ToList();
+                sut.Reset();
+
+                var actual = sut.ReadNewFiles();
+
+                actual.ShouldNotBeEmpty();
+            }
+
+            [Fact]
+            public void ImportControl_ZipHasOnlyOneEntryItsMarkedAsImported_ReturnsEmpty()
+            {
+                CreateZipAtFtp(@"fcs_regions\Adygeja_Resp\notifications\currMonth\", "panda.zip", "entry");
+                var sut = CreateSut();
+                sut.ReadNewFiles().ToList();
+                sut.MarkImported("ftp://localhost/fcs_regions/Adygeja_Resp/notifications/currMonth/panda.zip/entry");
+
+                var actual = sut.ReadNewFiles();
+
+                actual.ShouldBeEmpty();
+            }
+
+            [Fact]
+            public void ImportControl_ZipHasTwoEntriesOneMarkedAsImported_ReadsMarkedEntryAgain()
+            {
+                CreateZipAtFtp(@"fcs_regions\Adygeja_Resp\notifications\currMonth\",
+                               "panda.zip",
+                               zip =>
+                               {
+                                   zip.CreateEntry("yo");
+                                   zip.CreateEntry(Path.GetRandomFileName());
+                               });
+
+                var entryUrl = "ftp://localhost/fcs_regions/Adygeja_Resp/notifications/currMonth/panda.zip/yo";
+                var sut = CreateSut();
+                sut.ReadNewFiles().ToList();
+                sut.MarkImported(entryUrl);
+
+                var actual = sut.ReadNewFiles().ToList();
+
+                actual.ShouldContain(f => f.Uri == entryUrl);
+            }
+
+            [Fact]
+            public void ImportControl_ZipHadZeroLengthThenEntryWasAdded_WontReadItAgain()
+            {
+                var dir = @"fcs_regions\Adygeja_Resp\notifications\currMonth\";
+                var fileName = "panda.zip";
+                var path = PrepareForFile(dir, fileName);
+
+                File.WriteAllBytes(path, new byte[0]);
+                var sut = CreateSut();
+                sut.ReadNewFiles().ToList();
+
+                CreateZipAtFtp(dir, fileName, zip => zip.CreateEntry("entry"));
+
+                var actual = sut.ReadNewFiles();
+
+                actual.ShouldBeEmpty();
+            }
         }
 
-        [Theory]
-        [InlineData(@"fcs_regions\Adygeja_Resp\notifications\prevMonth\", "archive.zip", "entry",
-            @"ftp://localhost/fcs_regions/Adygeja_Resp/notifications/prevMonth/archive.zip/entry")]
-        public void Read_ZipInNotificationsPrevMonth_PathIsUriPlusEntryName(string targetDirectory, string zipName, string entryName, string expected)
+        public class BadStuff : FileReaderTests
         {
-            CreateZipAtFtp(targetDirectory, zipName, entryName);
-            var sut = CreateSut();
+            [Fact]
+            public void Read_BadZip_ReturnsEmpty()
+            {
+                var fullDirPath = Path.Combine(FtpMountDir, @"fcs_regions\Adygeja_Resp\notifications\currMonth\");
+                Directory.CreateDirectory(fullDirPath);
+                File.WriteAllText(Path.Combine(fullDirPath, "bad.zip"), "bad zip content");
+                var sut = CreateSut();
 
-            var actual = sut.ReadNewFiles();
+                var actual = sut.ReadNewFiles();
 
-            actual.Single().Uri.ShouldBe(expected);
-        }
+                actual.ShouldBeEmpty();
+            }
 
-        [Fact]
-        public void Read_ZipInNotificationsCurrentMonth_ReadsItsContent()
-        {
-            var content = "<привет></привет>";
-            CreateZipAtFtp(@"fcs_regions\Adygeja_Resp\notifications\currMonth\",
-                           "panda.zip",
-                           zip =>
-                           {
-                               var entry = zip.CreateEntry(Path.GetRandomFileName());
-                               using (var stream = new StreamWriter(entry.Open()))
-                                   stream.Write(content);
-                           });
-            var sut = CreateSut();
+            [Fact]
+            public void Read_BadZipAndGoodZip_ReturnsGoodZip()
+            {
+                var currMonth = @"fcs_regions\Adygeja_Resp\notifications\currMonth\";
+                CreateZipAtFtp(currMonth, "good.zip", Path.GetRandomFileName());
+                var fullDirPath = Path.Combine(FtpMountDir, currMonth);
+                Directory.CreateDirectory(fullDirPath);
+                File.WriteAllText(Path.Combine(fullDirPath, "bad.zip"), "bad zip content");
+                var sut = CreateSut();
 
-            var actual = sut.ReadNewFiles();
+                var actual = sut.ReadNewFiles();
 
-            actual.Single().Content.ShouldBe(content);
-        }
+                actual.Count().ShouldBe(1);
+            }
 
-        [Fact]
-        public void ImportControl_ZipWasntMarkedAsImported_ReadsItAgain()
-        {
-            CreateZipAtFtp(@"fcs_regions\Adygeja_Resp\notifications\currMonth\", "panda.zip", Path.GetRandomFileName());
-            var sut = CreateSut();
-            sut.ReadNewFiles().ToList();
+            [Fact]
+            public void Read_Always_IgnoresLogsDir()
+            {
+                var logsDir = @"fcs_regions\_logs\notifications\currMonth\";
+                CreateZipAtFtp(logsDir, "some.zip", Path.GetRandomFileName());
+                var sut = CreateSut();
 
-            var actual = sut.ReadNewFiles();
+                var actual = sut.ReadNewFiles();
 
-            actual.ShouldNotBeEmpty();
-        }
+                actual.ShouldBeEmpty();
+            }
 
-        [Fact]
-        public void ImportControl_ReaderWasReset_ReadsAgain()
-        {
-            CreateZipAtFtp(@"fcs_regions\Adygeja_Resp\notifications\currMonth\", "panda.zip", Path.GetRandomFileName());
-            var sut = CreateSut();
-            sut.ReadNewFiles().ToList();
-            sut.Reset();
+            [Fact]
+            public void Read_FailedToDownloadFile_ReturnsEmpty()
+            {
+                var sut = new FileReaderTestable(new Uri("ftp://localhost"));
+                sut.GetFileCoreBody = (client, uri) => { throw new WebException(); };
+                CreateZipAtFtp(@"fcs_regions\Adygeja_Resp\notifications\currMonth\", "some.zip", Path.GetRandomFileName());
 
-            var actual = sut.ReadNewFiles();
+                var actual = sut.ReadNewFiles();
 
-            actual.ShouldNotBeEmpty();
-        }
+                actual.ShouldBeEmpty();
+            }
 
-        [Fact]
-        public void ImportControl_ZipHasOnlyOneEntryItsMarkedAsImported_ReturnsEmpty()
-        {
-            CreateZipAtFtp(@"fcs_regions\Adygeja_Resp\notifications\currMonth\", "panda.zip", "entry");
-            var sut = CreateSut();
-            sut.ReadNewFiles().ToList();
-            sut.MarkImported("ftp://localhost/fcs_regions/Adygeja_Resp/notifications/currMonth/panda.zip/entry");
+            [Fact]
+            public void Read_FailedToDownloadFile_ContinueToNextFile()
+            {
+                var sut = new FileReaderTestable(new Uri("ftp://localhost"));
+                var shouldThrow = true;
+                sut.GetFileCoreBody = (client, uri) =>
+                {
+                    if (shouldThrow)
+                    {
+                        shouldThrow = false;
+                        throw new WebException();
+                    }
+                    return GetValidNonEmptyZipContent();
+                };
+                CreateZipAtFtp(@"fcs_regions\Adygeja_Resp\notifications\currMonth\", "some1.zip", Path.GetRandomFileName());
+                CreateZipAtFtp(@"fcs_regions\Adygeja_Resp\notifications\currMonth\", "some2.zip", Path.GetRandomFileName());
 
-            var actual = sut.ReadNewFiles();
+                var actual = sut.ReadNewFiles();
 
-            actual.ShouldBeEmpty();
-        }
-
-        [Fact]
-        public void ImportControl_ZipHasTwoEntriesOneMarkedAsImported_ReadsMarkedEntryAgain()
-        {
-            CreateZipAtFtp(@"fcs_regions\Adygeja_Resp\notifications\currMonth\",
-                           "panda.zip",
-                           zip =>
-                           {
-                               zip.CreateEntry("yo");
-                               zip.CreateEntry(Path.GetRandomFileName());
-                           });
-
-            var entryUrl = "ftp://localhost/fcs_regions/Adygeja_Resp/notifications/currMonth/panda.zip/yo";
-            var sut = CreateSut();
-            sut.ReadNewFiles().ToList();
-            sut.MarkImported(entryUrl);
-
-            var actual = sut.ReadNewFiles().ToList();
-
-            actual.ShouldContain(f => f.Uri == entryUrl);
-        }
-
-        [Fact]
-        public void ImportControl_ZipHadZeroLengthThenEntryWasAdded_WontReadItAgain()
-        {
-            var dir = @"fcs_regions\Adygeja_Resp\notifications\currMonth\";
-            var fileName = "panda.zip";
-            var path = PrepareForFile(dir, fileName);
-
-            File.WriteAllBytes(path, new byte[0]);
-            var sut = CreateSut();
-            sut.ReadNewFiles().ToList();
-
-            CreateZipAtFtp(dir, fileName, zip => zip.CreateEntry("entry"));
-
-            var actual = sut.ReadNewFiles();
-
-            actual.ShouldBeEmpty();
-        }
-
-        [Fact]
-        public void Read_BadZip_ReturnsEmpty()
-        {
-            var fullDirPath = Path.Combine(FtpMountDir, @"fcs_regions\Adygeja_Resp\notifications\currMonth\");
-            Directory.CreateDirectory(fullDirPath);
-            File.WriteAllText(Path.Combine(fullDirPath, "bad.zip"), "bad zip content");
-            var sut = CreateSut();
-
-            var actual = sut.ReadNewFiles();
-
-            actual.ShouldBeEmpty();
-        }
-
-        [Fact]
-        public void Read_BadZipAndGoodZip_ReturnsGoodZip()
-        {
-            var currMonth = @"fcs_regions\Adygeja_Resp\notifications\currMonth\";
-            CreateZipAtFtp(currMonth, "good.zip", Path.GetRandomFileName());
-            var fullDirPath = Path.Combine(FtpMountDir, currMonth);
-            Directory.CreateDirectory(fullDirPath);
-            File.WriteAllText(Path.Combine(fullDirPath, "bad.zip"), "bad zip content");
-            var sut = CreateSut();
-
-            var actual = sut.ReadNewFiles();
-
-            actual.Count().ShouldBe(1);
-        }
-
-        [Fact]
-        public void Read_Always_IgnoresLogsDir()
-        {
-            var logsDir = @"fcs_regions\_logs\notifications\currMonth\";
-            CreateZipAtFtp(logsDir, "some.zip", Path.GetRandomFileName());
-            var sut = CreateSut();
-
-            var actual = sut.ReadNewFiles();
-
-            actual.ShouldBeEmpty();
-        }
-
-        [Fact]
-        public void Read_FailedToDownloadFile_ReturnsEmpty()
-        {
-            var sut = new FileReaderTestable(new Uri("ftp://localhost"));
-            sut.GetFileCoreBody = (client, uri) => { throw new WebException(); };
-            CreateZipAtFtp(@"fcs_regions\Adygeja_Resp\notifications\currMonth\", "some.zip", Path.GetRandomFileName());
-
-            var actual = sut.ReadNewFiles();
-
-            actual.ShouldBeEmpty();
-        }
-
-        [Fact]
-        public void Read_FailedToDownloadFile_ContinueToNextFile()
-        {
-            var sut = new FileReaderTestable(new Uri("ftp://localhost"));
-            var shouldThrow = true;
-            sut.GetFileCoreBody = (client, uri) =>
-                                  {
-                                      if (shouldThrow)
-                                      {
-                                          shouldThrow = false;
-                                          throw new WebException();
-                                      }
-                                      return GetValidNonEmptyZipContent();
-                                  };
-            CreateZipAtFtp(@"fcs_regions\Adygeja_Resp\notifications\currMonth\", "some1.zip", Path.GetRandomFileName());
-            CreateZipAtFtp(@"fcs_regions\Adygeja_Resp\notifications\currMonth\", "some2.zip", Path.GetRandomFileName());
-
-            var actual = sut.ReadNewFiles();
-
-            actual.Count().ShouldBe(1);
+                actual.Count().ShouldBe(1);
+            }
         }
 
         private static byte[] GetValidNonEmptyZipContent()
@@ -246,7 +255,7 @@ namespace Overseer.Doorkeeper.Tests
             }
         }
 
-        private FileReader CreateSut()
+        private static FileReader CreateSut()
         {
             return new FileReader(new Uri("ftp://localhost"));
         }
